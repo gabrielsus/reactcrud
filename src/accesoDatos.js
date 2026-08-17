72fd5033d3750c8824f5856ff6b1e3108b0fefbf
gabrielsus <Gabriel.sus@gmail.com>
2026-08-17 17:59:13 -0300

    spa

--- src/accesoDatos.js
+++ src/accesoDatos.js
@@ -9,3 +9,30 @@ export const obtenerAdministrador = async (matricula) => {
         throw error;
     }   
 }
+
+export const obtenerPDF = async (ticker,desde,hasta)=> {
+    try {
+        const response = await axios.post('http://localhost:8000/descargar-reporte-pedf/',
+        {
+            ticker: ticker,
+            desde: desde,
+            hasta: hasta
+        },
+        {
+            responseType: 'blob', // Indica que la respuesta será un archivo binario
+        }
+        );
+        const blob = new Blob([response.data], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'reporte.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url); // Libera el objeto URL creado
+    } catch (error) {
+        console.error('Error al obtener el PDF:', error);
+        throw error;
+    }
+}
